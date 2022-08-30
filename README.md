09e5594d4ed835218add4968007fe5555963537b
Gustavo Dell Anhol Oliveira <gustavo.dellanhol@gmail.com>
2022-08-29 23:18:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Freeway clone whit p5js
+ This is a clone of the Freeway Atari game, made with study purposes
---
+++ script.js
@@ -0,0 +1,16 @@
+print('Access-Control-Allow-Origin: *');
+
+let bg_image
+
+function preload(){
+  bg_image = loadImage('http://gustavooliveira.byethost33.com/p5Js/freeway_assets/assets/road.png')
+}
+
+function setup() {
+  createCanvas(640,480)
+}
+
+function draw() {
+  background(0)
+  
+}
